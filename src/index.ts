7d2b3f331e8cff580a0281ac477ca914ca5e7958
Palm <wasin.ho@logicspark.com>
2023-08-18 15:03:13 +0700

    open export generateSidebar

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { generateSidebar } from "./sidebar-generator";
 import ScrollUtils from "./utils/scroll-check-header";
-import FileHelper from "./utils/file-helper";
 
 export default function VitepressPluginNestedSidebar() {
   const { calculateAndHighlightHeader, resetHeader } = ScrollUtils;
@@ -8,7 +7,7 @@ export default function VitepressPluginNestedSidebar() {
     return filename.replace(/\.md$/, "");
   };
   return {
-    // generateSidebar,
+    generateSidebar,
     calculateAndHighlightHeader,
     getFilename,
     resetHeader,
